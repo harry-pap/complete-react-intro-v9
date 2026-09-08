e764f2632fba607c8cccb4c87ff6ddc4d4443e0c
Charalampis Papadopoulos <cha.papadopoulos@kaizengaming.com>
2026-09-08 16:04:50 +0300

    add snapshot testing

---
+++ src/__tests__/Cart.test.jsx
@@ -0,0 +1,21 @@
+import { expect, test } from "vitest";
+import { render } from "@testing-library/react";
+import Cart from "../Cart";
+
+test("snapshot with empty cart", async () => {
+  const { asFragment } = render(<Cart cart={[]} />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test("snapshot with items in cart", async () => {
+  const testPizza = {
+    id: "hawaiian",
+    name: "The great Hawaiian",
+    category: "Supreme",
+    description: "The great pizza with Pineapple",
+    image: "/public/images/hawaiian.webp",
+    sizes: { S: 25, M: 30, L: 50 },
+  };
+
+  expect(testPizza).toMatchSnapshot();
+});
